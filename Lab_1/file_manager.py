fda6f6240190d8e0125a6788ebb8d1785eea2acf
m1st0rm <mishaboy333@gmail.com>
2024-09-16 16:00:12 +0300

    Renamed file_reader.py to file_manager.py and added write_file method

--- Lab_1/file_manager.py
+++ Lab_1/file_manager.py
@@ -7,3 +7,8 @@ def read_file(file_path: str) -> str:
         content += " " * (8 - remainder)
 
     return content
+
+
+def write_file(file_path: str, content: str) -> None:
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(content)

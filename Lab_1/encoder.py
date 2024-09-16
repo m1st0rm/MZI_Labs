a7b388125e4a116cac6d3914f67d0499e947ab4c
m1st0rm <mishaboy333@gmail.com>
2024-09-16 18:41:16 +0300

    Updated encoder.py

--- Lab_1/encoder.py
+++ Lab_1/encoder.py
@@ -25,3 +25,24 @@ def key_looper_decrypt(key_bits: List[bitarray]) -> List[bitarray]:
         keys_looped.append(key_bits[(31 - i) % 8])
 
     return keys_looped
+
+
+def encrypt_bits(
+    text_bits: List[bitarray], key_unlooped_bits: List[bitarray]
+) -> List[bitarray]:
+    encrypted_bits = []
+    key_looped_bits = key_looper_encrypt(key_unlooped_bits)
+
+    for i in range(0, len(text_bits)):
+        encrypted_bits.append(
+            encryption_function(text_bits[i][:32], text_bits[i][32:], key_looped_bits)
+        )
+
+    return encrypted_bits
+
+
+def encryption_function(
+    bits_block_1: bitarray, bits_block_2: bitarray, key: List[bitarray]
+) -> List[bitarray]:
+    for i in range(0, 32):
+        pass

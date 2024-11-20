f48429f8d4731abc4b9d97f3cd1e8e3ddccdfff3
m1st0rm <mishaboy333@gmail.com>
2024-11-20 22:06:32 +0100

    Little changes in Lab 6

--- Lab_6/asn.py
+++ Lab_6/asn.py
@@ -7,60 +7,47 @@ def encode_file_signature(Q, p, curve, P, q, r, s):
 
     struct.start()
 
-    # Sequence_1 start
     struct.enter(asn1.Numbers.Sequence)
 
-    # Set_1 start
     struct.enter(asn1.Numbers.Set)
 
-    # Sequence_2 start
     struct.enter(asn1.Numbers.Sequence)
 
-    # \x00\x06\x07\x00 - GOST3410-2012
     struct.write(b"\x80\x06\x07\x00", asn1.Numbers.OctetString)
     struct.write(b"gostSignKey ", asn1.Numbers.UTF8String)
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.write(Q.x, asn1.Numbers.Integer)
     struct.write(Q.y, asn1.Numbers.Integer)
     struct.leave()
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.write(p, asn1.Numbers.Integer)
     struct.leave()
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.write(curve.a, asn1.Numbers.Integer)
     struct.write(curve.b, asn1.Numbers.Integer)
     struct.leave()
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.write(P.x, asn1.Numbers.Integer)
     struct.write(P.y, asn1.Numbers.Integer)
     struct.leave()
 
     struct.write(q, asn1.Numbers.Integer)
-    # Sequence_2 end
     struct.leave()
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.write(r, asn1.Numbers.Integer)
     struct.write(s, asn1.Numbers.Integer)
     struct.leave()
 
-    # Set_1 end
     struct.leave()
 
-    #
     struct.enter(asn1.Numbers.Sequence)
     struct.leave()
 
-    # Sequence_1 end
     struct.leave()
 
     return struct.output()
